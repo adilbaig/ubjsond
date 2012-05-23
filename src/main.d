40776ae5c23ab40ea6925d03c210dd525568fe16
Adil <adil@stockopedia.co.uk>
2012-05-24 00:45:38 +0530

    Get the value from an element

--- src/main.d
+++ src/main.d
@@ -5,11 +5,14 @@ import std.stdio,
 
 void main(string[] args)
 {
-    auto e = array("PHP", "Javascript");
-    auto c = array("Adil", 29, 29.786, e);
+    auto e = arrayElement("PHP", "Javascript");
+    auto c = arrayElement("Adil", 29, 29.786, e);
     auto o = objectElement("Name", "Adil", "Age", 29, "Score", 289.123);
     
     writeln(c);
     writeln(decode(c.bytes)[0]);
     writeln(o);
+    
+    auto b = elements(true, int.max, null);
+    writeln(b, b[1].value!int, ',', cast(int)b[1]);
 }
--- src/ubjson/ubjson.d
+++ src/ubjson/ubjson.d
@@ -16,8 +16,8 @@ enum Type : char {
     Int64 = 'L',
     Float = 'd',
     Double = 'D',
-//    HugeSmall = 'h', //Not supported
-//    HugeLarge = 'H', //Not supported
+    HugeSmall = 'h',
+    HugeLarge = 'H', 
     StringSmall = 's',
     StringLarge = 'S',
     ObjectSmall = 'o',
@@ -41,6 +41,74 @@ struct Element {
         Element[] array;
     }
     
+    bool isNull()
+    {
+        return (type == Type.Null);
+    }
+    
+    /**
+     * Get the value stored in this Element. 
+     */
+    T value(T)()
+    {
+        static if(is(T == bool))
+        {
+            if(!(type == Type.True || type == Type.False))
+                throw new IncompatibleCastException("Cannot convert " ~ type ~ " to a boolean");
+                
+            return (type == Type.True);
+        }
+        else static if(is(T == byte)){
+            if(type != Type.Byte)
+                throw new IncompatibleCastException("Cannot convert " ~ type ~ " to a byte");
+            
+            ubyte[1] temp = data;
+            return bigEndianToNative!(byte)(temp);
+        }
+        else static if(is(T == short)){
+            if(type != Type.Int16)
+                throw new IncompatibleCastException("Cannot convert " ~ type ~ " to a short");
+            
+            ubyte[2] temp = data;
+            return bigEndianToNative!(short)(temp);
+        }
+        else static if(is(T == int)){
+            if(type != Type.Int32)
+                throw new IncompatibleCastException("Cannot convert " ~ type ~ " to an int");
+            
+            ubyte[4] temp = data;
+            return bigEndianToNative!(int)(temp);
+        }
+        else static if(is(T == long))
+        {
+            if(type != Type.Int64)
+                throw new IncompatibleCastException("Cannot convert " ~ type ~ " to a long");
+
+            ubyte[8] temp = data;
+            return bigEndianToNative!(long)(temp);
+        }
+        else static if(is(T == float))
+        {
+            if(type != Type.Float)
+                throw new IncompatibleCastException("Cannot convert " ~ type ~ " to a float");
+
+            ubyte[4] temp = data;
+            return bigEndianToNative!(float)(temp);
+        }
+        else static if(is(T == double))
+        {
+            if(type != Type.Double)
+                throw new IncompatibleCastException("Cannot convert " ~ type ~ " to a double");
+
+            ubyte[8] temp = data;
+            return bigEndianToNative!(double)(temp);
+        }
+        else static if(is(T == string))
+        {
+            return toString();
+        }
+    }
+    
     @property string toString()
     {
         string ret;
@@ -54,23 +122,27 @@ struct Element {
             case Type.False:
                 return "false";
             case Type.Byte:
-                ubyte[1] temp = data[0 .. 1];
+                ubyte[1] temp = data;
                 return to!string(bigEndianToNative!(byte)(temp));
             case Type.Int16:
-                ubyte[2] temp = data[0 .. $];
+                ubyte[2] temp = data;
                 return to!string(bigEndianToNative!(short)(temp));
             case Type.Int32:
-                ubyte[4] temp = data[0 .. $];
+                ubyte[4] temp = data;
                 return to!string(bigEndianToNative!(int)(temp));
             case Type.Int64:
-                ubyte[8] temp = data[0 .. $];
+                ubyte[8] temp = data;
                 return to!string(bigEndianToNative!(long)(temp));
             case Type.Float:
-                ubyte[4] temp = data[0 .. $];
+                ubyte[4] temp = data;
                 return to!string(bigEndianToNative!(float)(temp));
             case Type.Double:
-                ubyte[8] temp = data[0 .. $];
+                ubyte[8] temp = data;
                 return to!string(bigEndianToNative!(double)(temp));
+            
+            case Type.HugeSmall:
+            case Type.HugeLarge:
+                return cast(string)data;
             
             case Type.StringSmall:
             case Type.StringLarge:
@@ -123,13 +195,24 @@ struct Element {
         
         return t;
     }
+    
+    
+    T opCast(T)() { return value!T; }
+
+    //opAssign
+    //opIndex, opSlice for arrays and objects
+}
+
+class IncompatibleCastException : Exception
+{
+    this(string message){ super(message); }
 }
 
 /**
  * Given a variable set of parameters encodes the data to Element[].
  * Args can be : strings, integers, floats, bools, nulls or Elements
  */
-Element[] encodeToElements(T...)(T args)
+Element[] elements(T...)(T args)
 {
     Element[] e;
     foreach(a; args)
@@ -140,13 +223,13 @@ Element[] encodeToElements(T...)(T args)
 
 /**
  * Given a variable set of parameters encodes the data to UBJSON format.
- * Args can be anything that can be decoded by encodeToElements
+ * Args can be anything that can be decoded by elements()
  */
 immutable(ubyte)[] encode(T...)(T args)
 {
     immutable(ubyte)[] bytes;
     
-    Element[] elements = encodeToElements(args);
+    Element[] elements = elements(args);
     foreach(e; elements)
         bytes ~= e.bytes;
         
@@ -173,11 +256,11 @@ Element[] decode(in immutable(ubyte)[] bytes)
 
 /**
  * Creates a UBJSON array (Element) from the given args.
- * Args can be anything that can be decoded by encodeToElements
+ * Args can be anything that can be decoded by elements
  */
-Element array(T...)(T args)
+Element arrayElement(T...)(T args)
 {
-    Element[] elements = encodeToElements(args);
+    Element[] elements = elements(args);
         
     auto e = Element((elements.length <= ubyte.max) ? Type.ArraySmall : Type.ArrayLarge, cast(uint)elements.length);
     e.array = elements;
@@ -187,14 +270,14 @@ Element array(T...)(T args)
 
 /**
  * Creates a UBJSON Object (Element) from the given args.
- * Args can be anything that can be decoded by encodeToElements
+ * Args can be anything that can be decoded by elements
  */
 Element objectElement(T...)(T args)
 {
     if(args.length % 2 > 0)
         throw new Exception("Object is incomplete");
     
-    Element[] elements = encodeToElements(args);
+    Element[] elements = elements(args);
         
     auto l =  cast(uint)elements.length/2;
     auto e = Element((l <= ubyte.max) ? Type.ObjectSmall : Type.ObjectLarge, l);
@@ -236,12 +319,14 @@ private :
             case Type.Int64:
             case Type.Double:
                 return Element(cast(Type)c, 0, bytes[pointer .. pointer+8].idup);
-                
+            
+            case Type.HugeSmall:    
             case Type.StringSmall:
                 ubyte l = bytes[pointer .. pointer + 1][0];
                 pointer++;
                 return Element(cast(Type)c, l, bytes[pointer .. pointer + l].idup);
                 
+            case Type.HugeLarge:
             case Type.StringLarge:
                 ubyte size = 4;
                 ubyte[4] l = bytes[ pointer .. pointer + size];
