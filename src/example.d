3156cd4af3c3522a80b1e6f4f037db59002cfd26
Adil <adil@stockopedia.co.uk>
2012-05-24 02:23:42 +0530

    Unittests and comments

--- src/example.d
+++ src/example.d
@@ -5,6 +5,11 @@ import std.stdio,
 
 void main(string[] args)
 {
+    //Create a decent example showing encoding of static values, and creation of arrays/objects
+    //Decode sample values, print them and do type checks
+    //Append, delete array values
+    //Append, delete object properties
+    
     auto e = arrayElement("PHP", "Javascript");
     auto c = arrayElement("Adil", 29, 29.786, e);
     auto o = objectElement("Name", "Adil", "Age", 29, "Score", 289.123);
