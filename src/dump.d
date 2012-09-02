fb1406a11282a798eaf453b86fd2bc0a395e92e0
Adil <adil@stockopedia.co.uk>
2012-09-02 11:58:05 +0530

    Comment correction

--- src/dump.d
+++ src/dump.d
@@ -18,7 +18,7 @@ void main(string[] args)
     /**
      Read and print one of the UBJ files. Ex:
     
-     ./test resources/CouchDB4k.ubj
+     ./dump resources/CouchDB4k.ubj
     */
 
     auto bytes = cast(immutable(ubyte)[]) read(args[1]);
